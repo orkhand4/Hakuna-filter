3472de9e8e7c097fdb4cb12a39ac3e3f3f5764c5
Orkhand4 <orkhand4@mail.ru>
2024-09-22 11:05:38 +0400

    filter search

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ const data = [
 ];
 
 const inpt_search = document.querySelector('#search')
+const result_ul = document.querySelector('.search-result')
 
 inpt_search.addEventListener('input', () => {
   const inputText = inpt_search.value //inputda yazdigim
@@ -33,6 +34,19 @@ inpt_search.addEventListener('input', () => {
   const result = data.filter((item) =>{
     return item.name.toUpperCase().includes(inputText.toUpperCase())
   });
-  console.log((result))
+
+result_ul.innerHTML = '';
+
+if(inputText == '') {
+  result_ul.innerHTML = '<li>No Result</li>';
+}
+else {
+  result.map((item) => {
+    const li_tag = document.createElement('li');
+    li_tag.innerText = item.name;
+    result_ul.appendChild(li_tag);
+  });
+}
 })
+
   
